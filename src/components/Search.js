83284e8d6ce6e6dd49da755fce5c4be601b8c9a0
sidhi-sid <sidhisidh469@gmail.com>
2020-06-25 20:51:39 +0530

    search component

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Search extends React.Component{
+    state={
+        username:''
+    }
+    
+    handleUserNameChange=e=>{
+        const value=e.target.value;
+
+        this.setState({
+            username:value,
+        });
+    };
+    
+    render(){
+        const {username}=this.state;
+        return <input value={username} onChange={this.handleUserNameChange} type="text" name="username" placeholder="Enter username"></input>
+    }
+}
+
+export default Search;
