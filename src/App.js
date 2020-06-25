3361cd34198a39507bb34404400bc7ebf37dddbc
sidhi-sid <sidhisidh469@gmail.com>
2020-06-25 21:58:57 +0530

    search done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React from 'react';
 import Search from './components/Search'
 import './App.css';
 
-function App() {
-  return (
-    <Search/>
-  );
+class App extends React.Component {
+  state={
+    user:null
+  }
+  fetchUserData= async username=>{
+  //fetch github api
+  try{
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    if(res.ok){
+      const data=await res.json();
+      return this.setState({
+        user:data
+      })
+    }
+  }catch(err){
+    console.log(err)
+  }
+}
+
+  render(){
+    return <Search fetchData={this.fetchUserData}/>
+  } 
 }
 
 export default App;
